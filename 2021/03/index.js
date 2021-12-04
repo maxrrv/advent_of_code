bffccee1dd28c36f2ec313194c413af361e11b2a
Max Voelker <maxrru.developer@gmail.com>
2021-12-04 01:29:27 +0100

    add attempt for 2021/03-2

--- 2021/03/index.js
+++ 2021/03/index.js
@@ -2,25 +2,65 @@ const { default: httpsPromise } = require('../httpsPromise')
 const { debug, printResult } = require('../prettyPrint')
 
 const solutionTaskOne = data => {
-  let gammaRate = ''
-  let epsilonRate = ''
+  let [gammaRate, epsilonRate] = getMaxMinPattern(data)
+  return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2)
+}
 
-  const getMajorityCount = (data, amountOfOnes) => amountOfOnes < data.length/2 ? '0' : '1'
-  const getMinorityCount = (data, amountOfOnes) => amountOfOnes < data.length/2 ? '1' : '0'
+const solutionTaskTwo = data => {
+  data = [
+'00100',
+'11110',
+'10110',
+'10111',
+'10101',
+'01111',
+'00111',
+'11100',
+'10000',
+'11001',
+'00010',
+'01010',
+  ]
+  let [oxygenPattern, co2Pattern] = getMaxMinPattern(data)
 
-  for(let binaryIndex = 0; binaryIndex < data[0].length; binaryIndex++) {
+  const oxygenRating = filterByPattern(oxygenPattern, data)
+  const co2Rating = filterByPattern(co2Pattern, data)
+
+  return parseInt(oxygenRating, 2) * parseInt(co2Rating, 2)
+}
+
+const filterByPattern = (pattern, listToFilter) => {
+  let list = listToFilter
+  let index = 0
+
+  while(list.length > 1) {
+    list = list.filter(element => element[index] === pattern[index])
+    index++
+  }
+
+  return list[0]
+}
+
+const getMajorityCount = (data, amountOfOnes) => amountOfOnes < Math.round(data.length/2) ? '0' : '1'
+const getMinorityCount = (data, amountOfOnes) => amountOfOnes < Math.round(data.length/2) ? '1' : '0'
+
+const getMaxMinPattern = (itemList) => {
+  maxPattern = ''
+  minPattern = ''
+  for(let binaryIndex = 0; binaryIndex < itemList[0].length; binaryIndex++) {
     const countAllOnesInColumn = (countOfOnes, currentBinaryString) => {
       return parseInt(currentBinaryString[binaryIndex]) === 1 ? ++countOfOnes : countOfOnes
     }
 
-    const amountOfOnes = data.reduce(countAllOnesInColumn, 0)
+    const amountOfOnes = itemList.reduce(countAllOnesInColumn, 0)
 
-    gammaRate += getMajorityCount(data, amountOfOnes)
-    epsilonRate += getMinorityCount(data, amountOfOnes)
+    maxPattern += getMajorityCount(itemList, amountOfOnes)
+    minPattern += getMinorityCount(itemList, amountOfOnes)
   }
-
-  printResult('result: ', parseInt(gammaRate, 2) * parseInt(epsilonRate, 2))
+  return [maxPattern, minPattern]
 }
 
-httpsPromise('/2021/day/3/input').then(solutionTaskOne)
+httpsPromise('/2021/day/3/input')
+  .then(solutionTaskTwo)
+  .then(result => printResult('result: ', result))
 
