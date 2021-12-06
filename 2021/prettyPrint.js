91a3bf402992343d573c6947a9c5bbf4a19a476f
Max Voelker <maxrru.developer@gmail.com>
2021-12-06 23:37:49 +0100

    add function to print test results based on assertions

--- 2021/prettyPrint.js
+++ 2021/prettyPrint.js
@@ -24,10 +24,21 @@ const BgMagenta = "\x1b[45m"
 const BgCyan = "\x1b[46m"
 const BgWhite = "\x1b[47m"
 
-const printResult = (explanation, value) => console.log(`${Underscore}${explanation}${FgGreen}${value}`)
+const printResult = (explanation, value) => {
+  console.log(`${Underscore}${explanation}${FgGreen}${value}`)
+}
+const printTestResult = (expectedValue, actualValue) => {
+  const resultColor = expectedValue === actualValue ? FgGreen : FgRed
+  console.log(`
+    ${FgYellow}expected: ${expectedValue},
+    ${resultColor}got: ${actualValue}.
+    Result: ${expectedValue === actualValue}
+  `)
+}
 
 const debug = valueToDebug => console.log(`${FgYellow}${valueToDebug}`)
 
 module.exports.printResult = printResult
+module.exports.printTestResult = printTestResult
 module.exports.debug = debug
 
